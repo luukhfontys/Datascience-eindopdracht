16785ebd5ab146c992aae6e72b6e0d1a669165ee
Sammas04 <sam.aarts.04@gmail.com>
2024-05-23 12:09:12 +0200

    kort

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sp
 import re
 
 def load_data_index(data_path: str, bearing: int, index: int):
@@ -12,11 +13,15 @@ def load_data_index(data_path: str, bearing: int, index: int):
 def get_features(df: str):
     mean = [df.iloc[:, 0].mean(), df.iloc[:, 1].mean()]
     std = [df.iloc[:, 0].std(), df.iloc[:, 1].std()]
-    rms = [(sum(df.df.iloc[:, 0]**2)/len(df.df.iloc[:, 0]))**0.5, (sum(df.df.iloc[:, 1]**2)/len(df.df.iloc[:, 1]))**0.5]
-    
-    return mean + std + rms
+    rms = [(sum(df.iloc[:, 0]**2)/len(df.iloc[:, 0]))**0.5, (sum(df.iloc[:, 1]**2)/len(df.iloc[:, 1]))**0.5]
+    kurtosis = [sp.kurtosis(df.iloc[:,0]), sp.kurtosis(df.iloc[:,1])]
+    return mean + std + rms + kurtosis
 
 
 
 df = load_data_index('train', 1, 0)
+
+
+print(get_features(df))
+
 x=1
