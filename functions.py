e5f68b77aa0407426215c6d7785dabd84285f988
Sammas04 <sam.aarts.04@gmail.com>
2024-05-23 12:03:07 +0200

    sync

--- functions.py
+++ functions.py
@@ -9,14 +9,12 @@ def load_data_index(data_path: str, bearing: int, index: int):
     df_csv_filtered = df_csv[[col for col in df_csv.columns if str(bearing) in col]]
     return df_csv_filtered
 
-def get_features(df_filtered_index: str):
+def get_features(df: str):
     mean = [df.iloc[:, 0].mean(), df.iloc[:, 1].mean()]
     std = [df.iloc[:, 0].std(), df.iloc[:, 1].std()]
-    rms = [(sum(df.df.iloc[:, 0]**2)/len(df.df.iloc[:, 0]))**0.5, (sum(df.df.iloc[:, 1]**2)/len(df.df.iloc[:, 1]))**0.5]
+    rms = [(sum(df.iloc[:, 0]**2)/len(df.iloc[:, 0]))**0.5, (sum(df.iloc[:, 1]**2)/len(df.iloc[:, 1]))**0.5]
     
     return mean + std + rms
 
-
-
-df = load_data_index('train', 1, 0)
-x=1
+df_0 = pd.read_csv('train/0.csv',sep=';')
+print(get_features(df_0))
