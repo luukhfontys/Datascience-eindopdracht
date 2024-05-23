97b42831b43f4911d8e0f6a9b1bf342c2724d289
luukhfontys <l.hooymans@student.fontys.nl>
2024-05-23 12:08:19 +0200

    ffeatures

--- functions.py
+++ functions.py
@@ -13,7 +13,9 @@ def get_features(df: str):
     mean = [df.iloc[:, 0].mean(), df.iloc[:, 1].mean()]
     std = [df.iloc[:, 0].std(), df.iloc[:, 1].std()]
     rms = [(sum(df.iloc[:, 0]**2)/len(df.iloc[:, 0]))**0.5, (sum(df.iloc[:, 1]**2)/len(df.iloc[:, 1]))**0.5]
-
+    variance = [df.iloc[:, 0].var(), df.iloc[:, 1].var()]
+    crest_factor = [np.max(np.abs(df.iloc[:, 0])) / rms[0], np.max(np.abs(df.iloc[:, 1])) / rms[1]]
+    
     return mean + std + rms
 
 
