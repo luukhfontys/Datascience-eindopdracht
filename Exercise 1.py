29aeffadf4ae3ce21ef3da9f4e6890ae08ca9ac3
SamAarts <144012877+Sammas04@users.noreply.github.com>
2024-06-06 13:22:06 +0200

    dataset creeeren

--- Exercise 1.py
+++ Exercise 1.py
@@ -7,8 +7,13 @@ def main():
 
     # opg 1b
     dataset = generate_dataset(data_path)
-    dataset.to_excel('dataset_features.xlsx')
+    # dataset.to_excel('dataset_features.xlsx')
+    corr_matrix = dataset.corr()
 
+    # Plot the correlation matrix
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+    plt.show()
     
 
 main()
